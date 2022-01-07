118ea3d098b5c84d5f461179a115afe06742e301
jin-ryu <ann0905@naver.com>
2022-01-07 18:22:03 +0900

    commit greedy

---
+++ Algorithm/그리디/곱하기혹은더하기.java
@@ -0,0 +1,34 @@
+package Algorithm.그리디;
+
+import java.util.*;
+
+public class 곱하기혹은더하기 {
+    public static void main(String[] args) {
+        Scanner sc =  new Scanner(System.in);
+        String str = sc.next();
+
+        // 첫 번째 문자를 숫자로 변경한 값을 대임
+        long result = str.charAt(0) - '0';
+        for (int i = 1; i < str.length(); i++){
+            // 두 수 중에서 하나라도 '0' 혹은 '1'인 경우, 곱하기 보다는 더하기 수행
+            int num = str.charAt(i) - '0';
+            if(num <= 1 || result <= 1){
+                result += num;
+            }else{
+                result *= num;
+            }
+        }
+        System.out.println(result);
+    }
+}
+
+/*
+입력 예제1
+02984
+출력 예제1
+576
+입력 예제2
+567
+출력 예제2
+210
+*/
